dbf625bf93e22cbe9e043011be46477d0b85362c
mocne <kaifeng0605@sina.com>
2015-12-02 17:37:23 +0800

    创建项目工程

---
+++ AWKWARD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AWKWARD
+//
+//  Created by qingyun on 15/12/2.
+//  Copyright © 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
